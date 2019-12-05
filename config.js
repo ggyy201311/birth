2125c32985858b8a58f2941e5b48c9643003e9f8
matrix-01 <58478967+matrix-01@users.noreply.github.com>
2019-12-06 00:38:10 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,21 +1,19 @@
-// 霸都丶傲天 2019年10月10日 https://github.com/AJLoveChina/birthday
 var config = {
     // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
     // 每句话尽量不要超过15个字,不然展示效果可能不太好
     texts: [
-        "test",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
-         
-        "YAMI~~"
+        "test",
+        "end"
     ],
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "1test",
+        turn_on: "开始",
         play: "音乐",
         bannar_coming: "颜色",
         balloons_flying: "好像少点东西",
         cake_fadein: "蛋糕？",
         light_candle: "蜡烛？",
         wish_message: "生日快乐",
-        story: "A MESSAGE FOR YOU", 
+        story: "A MESSAGE FOR YOU",
     }
 };
