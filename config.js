a69d7c397d172a3fc2bcf153d48ca03417cb9849
ggyy201311 <494283984@qq.com>
2021-04-26 15:15:10 +0800

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ var config = {
        
     ],
     desc: {
-        turn_on: "送给世界上最可爱的赖艳能",
+        turn_on: "送给世界上最可爱的赖赖小姐。。",
         play: "音乐",
         bannar_coming: "颜色",
         balloons_flying: "好像少点东西",
